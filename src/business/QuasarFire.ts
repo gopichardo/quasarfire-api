6871f641692b55df9c736f65d413d1a7b31e8581
Cesar <gopichardoces@gmail.com>
2021-11-19 11:39:31 -0600

    Se agrega TypeScript

---
+++ src/business/QuasarFire.ts
@@ -0,0 +1,20 @@
+"use strict"
+
+class QuasarFire {
+
+    constructor() {
+
+    }
+
+    /**
+     * 
+     * @param distances Distances from emitter
+     * @
+     * Returns the Emiiter xy coordinates
+     */
+    GetLocation(distances: Float32Array[]) : any {
+
+    }
+}
+
+export default QuasarFire;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("Quasar Fire Operation ready man!");
