f3834ec981b6514cdf3d0d2b55a6184208f35c17
Cesar GONZALEZ <cesar.gonzalez@consulting-for.edenred.com>
2021-11-18 21:58:21 -0600

    Se agrega trilateracion

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+//---------------------------
+
+const { calculate, Vector } = require("weighted-positioning");
+
+const kenobi = new Vector(-500, -200);
+const skywalker = new Vector(100, -100);
+const sato = new Vector(500, 100);
+
+let res = calculate([
+  { v: kenobi, w: 100 },
+  { v: skywalker, w: 115.5 },
+  { v: sato, w: 142.7 },
+]);
+
+console.log(
+  "weighted-positioning - Coordenadas de la nave: X=",
+  res.x,
+  " Y=",
+  res.y
+);
+
+function GetLocation(distances) {}
